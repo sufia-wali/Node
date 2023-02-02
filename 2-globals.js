179063d8fa0074cb520649aa356d6d9397de1e24
sufia-wali <sufiawali1998@gmail.com>
2023-02-02 09:43:16 +0530

    first commit

---
+++ 2-globals.js
@@ -0,0 +1,11 @@
+//GLOBALS - NO WINDOW !!!
+
+//__dirname - path to current directory
+//__filename - file name
+// require - function to use modules(Comoon JS)
+// module - info about current module (File)
+// process - info about env where the program is being executed.
+console.log(__dirname);
+setInterval(()=>{
+  console.log('hello world');
+}, 1000)
---
+++ app.js
@@ -0,0 +1,3 @@
+const _ = require('lodash');
+const items = [1,[2,[3,[4]]]];
+console.log(_.flattenDeep(items));
